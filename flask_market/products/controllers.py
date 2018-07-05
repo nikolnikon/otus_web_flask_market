97236c9474a5bfa51918ad23efb5e25a8cb85727
nikolnikon <nikolnikon@gmail.com>
2018-07-05 17:33:31 +0300

    Сделал рендер шаблонов через jinja

--- flask_market/products/controllers.py
+++ flask_market/products/controllers.py
@@ -4,18 +4,20 @@ from flask_market.products.models import Product
 
 
 @products_app.route('/')
-def products():
-    return 'HREN!'
+def get_products_list():
+    return render_template('products/products_list.html')
 
 
 @products_app.route('/<int:id>')
-def product(id):
-    if not id:
-        # Запросить список товаров из БД
-        # Отрендерить страницу со списком товаров
-        pass
-    else:
-        # Запросить из БД конкретный товар
-        # Отрендерить страницу одного товара
-        product = Product.query.get_or_404(id)
-        # return render_template('products/product.htm', product=product)
+def get_product(id):
+    # if not id:
+    #     # Запросить список товаров из БД
+    #     # Отрендерить страницу со списком товаров
+    #     pass
+    # else:
+    #     # Запросить из БД конкретный товар
+    #     # Отрендерить страницу одного товара
+    #     product = Product.query.get_or_404(id)
+    #     # return render_template('products/product.htm', product=product)
+
+    return render_template('products/product.html')
